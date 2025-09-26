c3ec111429d2832c6c3c574c2278f9a05e5e63d4
alyoza.es <alyoza.es@tecnologicoargos.edu.ec>
2025-09-26 00:18:07 +0000

    clase_25092025

--- main.py
+++ main.py
@@ -14,3 +14,57 @@ es_estudiante = True #Booleano(True o False)
 print(f"Es estudiante: {es_estudiante}")
 print(type(es_estudiante))
 
+#Operadores aritméticos
+
+a = 1
+b = 2 
+
+resultado = a + b
+print(f"El resultado es: {resultado}")  
+
+resultado = a - b 
+print(f"El resultado es: {resultado}")
+
+resultado = a * b
+print(f"El resultado es: {resultado}")
+
+resultado = a / b
+print(f"El resultado es: {resultado}")
+
+a = 10 
+b = 3
+
+resultado = a // b 
+print(f"El resultado es: {resultado}") #Resultado 3
+
+a = -10
+b = 3
+
+resultado = a // b
+print(f"El resultado es: {resultado}") #Resultado -4
+
+a = 15
+b = 4
+
+resultado = a % b
+print(f"El resultado es: {resultado}") #Resultado 3
+
+a = 2
+b = 2
+resultado = a ** b
+print(f"El resultado es: {resultado}") #Resultado 4 
+
+#OPERADORES RELACIONALES: == !> > < >= <=
+
+# and or not
+
+#Asignaciones= = += -=
+
+
+
+
+
+
+
+
+
