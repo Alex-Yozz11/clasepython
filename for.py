221b54462ba1485e8c20d03718f62d3ff9efccd8
alyoza.es <alyoza.es@tecnologicoargos.edu.ec>
2025-10-01 00:40:36 +0000

    forclass

---
+++ for.py
@@ -0,0 +1,19 @@
+frutas = ["Manzana", "Banana", "Cereza"] #Arrays o arreglo es en plural
+
+for fruta in frutas:
+    print(fruta)
+
+for i in range(5): #del 0 al 4
+    print(i) 
+
+for i in range(len(frutas)): #len = dimension
+    print(frutas[i])
+
+for i in range(4): 
+    if i == 5:
+        break
+    if i % 2 == 0:
+        continue
+    print(i)
+else:
+    print("El bucle termino sin break")
