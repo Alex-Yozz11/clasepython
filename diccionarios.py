7f31727193b5e5b1f7241865b5e4162a54e16d8e
alyoza.es <alyoza.es@tecnologicoargos.edu.ec>
2025-10-08 00:42:54 +0000

    diccionarios

--- diccionarios.py
+++ diccionarios.py
@@ -1,3 +1,12 @@
 persona = {"Nombre": "Alex", "edad": 19, "Ciudad": "Guayaquil"}
 print(f"Diccionario inicial: {persona}")
 
+print("Edad", persona.get("edad"))
+persona ["profesion"] = "Ingeniero"
+print ("Persona con profesion:", persona)
+
+print("Claves", persona.keys()) #claves
+print("Valores", persona.values()) #valores
+
+persona["Ciudad"] = "Quito"
+print("Ciudad modificada", persona)
