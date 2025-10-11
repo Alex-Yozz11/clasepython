9ff59cd87cb6a53b37c7fdd1adca959abcf9c3c2
alyoza.es <alyoza.es@tecnologicoargos.edu.ec>
2025-10-11 15:56:03 +0000

    tarea2

---
+++ tarea2.py
@@ -0,0 +1,69 @@
+print("Primer parte de la tarea-filtrar y ordenar")
+entrada = [4, 7, 2, 4, 9, 2, 8, 6, 7]
+#No repetir numeros
+sin_repetir = []
+for n in entrada:
+    if n not in sin_repetir:
+        sin_repetir.append(n)
+
+print("Numeros sin reoetir: ", sin_repetir)
+
+#Numeros ordenados
+num_ordenado = sorted(sin_repetir)
+print("Números ordenados:", num_ordenado)
+
+#Mostrar numeros pares
+num_par = [n for n in num_ordenado if n % 2 == 0]
+print("Números pares:", num_par)
+
+print("\nSegunda parte de la tarea-tuplas")
+numeros = (5, 8, 12, 3, 9, 5, 2)
+# Calcular el maximo y el minimo
+maximo = max(numeros)
+minimo = min(numeros)
+
+print("Máximo:", maximo)
+print("Mínimo:", minimo)
+
+#Sacar promedio
+promedio = sum(numeros) / len(numeros)
+print("Promedio:", round(promedio, 2))
+
+# 3. Verificar si un número está dentro de la tupla
+num = int(input("Ingrese un número: "))
+
+if num in numeros:
+    print(f"El número {num} sí está en la tupla.")
+else:
+    print(f"El número {num} no está en la tupla.")
+
+print("\nTercera parte de la tarea-diccionarios")
+# Diccionario inicial
+inventario = {"manzanas": 10, "peras": 3, "naranjas": 8}
+
+#Agregar un nuevo producto
+nuevo_producto = input("Ingrese el nombre del nuevo producto: ").lower()
+cantidad = int(input("Ingrese la cantidad en stock: "))
+inventario[nuevo_producto] = cantidad
+
+#Actualizar cantidad de un producto existente
+producto_actualizar = input("Ingrese el nombre del producto que desea actualizar: ").lower()
+
+if producto_actualizar in inventario:
+    nueva_cantidad = int(input("Ingrese la nueva cantidad: "))
+    inventario[producto_actualizar] = nueva_cantidad
+else:
+    print(f"El producto '{producto_actualizar}' no está en el inventario.")
+
+#Mostrar productos con stock menor a 5
+print("\nProductos con stock bajo:")
+for producto, stock in inventario.items():
+    if stock < 5:
+        print(f"{producto} - {stock}")
+
+
+print("\nFin de la tarea 2")
+
+
+
+
